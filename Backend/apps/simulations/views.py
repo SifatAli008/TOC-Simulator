d9b105b3d1aa72c3693d95aa23f3faad592a2ffe
Shahdidul <theshahidkhan.2004@gmail.com>
2025-12-11 22:40:10 +0600

    migrated to django 6.0 to install django filter 25.2

--- Backend/apps/simulations/views.py
+++ Backend/apps/simulations/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
+from rest_framework import viewsets, status, filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from django.shortcuts import get_object_or_404
+from django.db.models import Q, Count, Prefetch
+from django.utils import timezone
+import logging
